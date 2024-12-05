eb265dd92c045e30550542770fb6eae4cbe7ead6
FNazemzade <F.S.Nazemzadeh@gmail.com>
2024-12-05 19:09:30 +0330

    app
    creating application

---
+++ WebSite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, JsonResponse
+
+def http_test(request):
+    return HttpResponse('<h1>this is a test</h1>')
+
+
+def json_test(request):
+    return JsonResponse({'name':'ali'})
